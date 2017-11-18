10e91386c7845ccb85905d232cb16e07d3073279
MarianaIgnowski <mariana-goto@hotmail.com>
2017-11-18 17:00:51 -0200

    Adição de novos arquivos para o funcionamento do upload de imagens #2

--- application/controllers/Imagem.php
+++ application/controllers/Imagem.php
@@ -1,8 +1,41 @@
 <?php
 class Imagem extends CI_Controller {
 
-	public function index(){
+	/*public function index(){
 		
 		echo "OLA MARILENE :)";
+	}*/
+
+	public function index(){
+		$this->load->helper('url');
+		$this->load->view('cad_img/cadastro');
 	}
+
+	public function salvar(){
+    $titulo = $this->input->post('titulo');
+   	$descricao = $this->input->post('descricao');
+    $curriculo = $_FILES['curriculo'];
+    $configuracao = array(
+        'upload_path'   => './imagens/',
+        'allowed_types' => 'png',
+        'file_name'     => date('Y-m-d').'.png',
+        'max_size'      => '500',
+        'max_width'     => '600',
+        'max_height'    => '600'
+
+    );      
+    $this->load->library('upload');
+    $this->upload->initialize($configuracao);
+    if ($this->upload->do_upload('curriculo')){
+       
+    	echo 'Arquivo salvo com sucesso.<br>';
+ 		echo 'O titulo da imagem: '.$titulo.'<br>';
+ 		echo 'A descrição da imagem: '.$descricao.'<br>';
+ 		
+    }else{
+        echo $this->upload->display_errors();
+    }
+
+	}
+
 }	
---
+++ application/views/cad_img/cadastro.php
@@ -0,0 +1,13 @@
+<form action="imagem/salvar" method="POST" enctype="multipart/form-data">
+
+   <input type="text" name="titulo" placeholder="Titulo"/> <br>
+   <input type="text" name="descricao" placeholder="Descicao"/>
+
+     <br/>
+     <input type="file" name="curriculo">
+     <br/>
+     <input type="submit" value="Salvar"/>
+</form>
+
+
+
